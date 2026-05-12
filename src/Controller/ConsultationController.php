eb36030a3586bbb90443ed754796594971d6c8be
anisnawarofficial <anisnawarofficial@gmail.com>
2026-05-12 14:37:54 +0200

    Update consultation UI

--- src/Controller/ConsultationController.php
+++ src/Controller/ConsultationController.php
@@ -8,6 +8,12 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class ConsultationController extends AbstractController
 {
+    #[Route('/consultation/en-communication', name: 'app_consultation_en_communication')]
+    public function enCommunication(): Response
+    {
+        return $this->render('pages/consultation/en-communication.html.twig');
+    }
+
     #[Route('/consultation/details', name: 'app_consultation_details')]
     public function details(): Response
     {
